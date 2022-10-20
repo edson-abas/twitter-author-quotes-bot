cbb86fd3b8b0ab918d77628bcbcf8595a526e72e
Edson Abas <115049679+edson-abas@users.noreply.github.com>
2022-10-19 22:32:41 -0300

    updated main.py

--- main.py
+++ main.py
@@ -31,27 +31,26 @@ class QuotesBot:
                                                           '.r-16y2uox.r-37j5jr.r-a023e6.r-b88u0q.r-1777fci.r-rjixqe'
                                                           '.r-bcqeeo.r-q4m81j.r-qvutc0')
         login.click()
-        time.sleep(6)
+        time.sleep(5)
 
         user_input = self.driver.find_element(By.XPATH, '//input[@autocomplete="username"]')
         user_input.send_keys(TWITTER_NAME)
         user_input.send_keys(Keys.ENTER)
-        time.sleep(6)
+        time.sleep(5)
 
         password_input = self.driver.find_element(By.NAME, "password")
         password_input.send_keys(PASSWORD)
         password_input.send_keys(Keys.ENTER)
-        time.sleep(6)
+        time.sleep(5)
 
         new_quote = self.get_quotes()
         tweet_input = self.driver.find_element(By.XPATH, "//div[contains(@aria-label, 'Texto do Tweet')]")
         tweet_input.send_keys(new_quote)
-        time.sleep(6)
 
         tweet_button = self.driver.find_element(By.XPATH, '//div[@data-testid="tweetButtonInline"]')
         tweet_button.click()
-        time.sleep(6)
-
+        
+        time.sleep(5)
         self.driver.quit()
 
 
